7430461bd4940aa36efb31bd13fad6d209c9ae6b
tiedebart <tiedebart@gmail.com>
2022-04-28 21:30:34 +0200

    initial commit

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const paintingsData = [
+    {
+      id: "aaa",
+      artistLastName: "Van Gogh",
+      artistFirstName: "Vincent",
+      title: "Horse",
+      year: 1886,
+      credits: "Van Gogh Museum Amsterdam"
+    },
+    {
+      id: "bbb",
+      artistLastName: "Kahlo",
+      artistFirstName: "Frida",
+      title: "Self-Portrait with Thorn Necklace and Hummingbird",
+      year: 1940,
+      credits: "fridakahlo.org"
+    },
+    {
+      id: "ccc",
+      artistLastName: "Kahlo",
+      artistFirstName: "Frida",
+      title: "Henry Ford Hospital (The Flying Bed)",
+      year: 1932,
+      credits: "fridakahlo.org"
+    },
+    {
+      id: "ddd",
+      artistLastName: "Van Gogh",
+      artistFirstName: "Vincent",
+      title: "Sunflowers",
+      year: 1889,
+      credits: "Van Gogh Museum Amsterdam"
+    },
+    {
+      id: "eee",
+      artistLastName: "Klee",
+      artistFirstName: "Paul",
+      title: "Urnenbild",
+      year: 1921,
+      credits: "Stedelijk Museum Amsterdam"
+    },
+    {
+      id: "fff",
+      artistLastName: "Klee",
+      artistFirstName: "Paul",
+      title: "Castle and Sun",
+      year: 1928,
+      credits: "Stedelijk Museum Amsterdam"
+    },
+    {
+      id: "ggg",
+      artistLastName: "Turner",
+      artistFirstName: "William",
+      title: "Dutch Boats in a Gale ('The Bridgewater Sea Piece')",
+      year: 1801,
+      credits: "nationalgallery.org.uk"
+    },
+    {
+      id: "hhh",
+      artistLastName: "Turner",
+      artistFirstName: "William",
+      title: "Calais Pier",
+      year: 1802,
+      credits: "nationalgallery.org.uk"
+    },
+    {
+      id: "iii",
+      artistLastName: "Kandinsky",
+      artistFirstName: "Wassily",
+      title: "Composition VIII",
+      year: 1923,
+      credits: "wassilykandinsky.net"
+    },
+    {
+      id: "jjj",
+      artistLastName: "Kandinsky",
+      artistFirstName: "Wassily",
+      title: "Murnau Street with Women",
+      year: 1908,
+      credits: "wassilykandinsky.net"
+    }
+  ]
---
+++ src/routes/index.svelte
@@ -0,0 +1,33 @@
+<script>
+    import {paintingsData} from '../data.js';
+
+    console.log(paintingsData.length);
+
+    let paintingsLength = paintingsData.length;
+    let randomNumber = Math.floor(Math.random() * paintingsLength);
+    let paintingPrompt = paintingsData[randomNumber];
+
+    let shuffledPaintings = paintingsData.sort(() => 0.5 - Math.random());
+    let chosenPainting = shuffledPaintings[0];
+
+    console.log('de gekozen bitch is' + chosenPainting.title);
+
+    let paintingPromptPath = `./img/pixelated_full/${chosenPainting.id}.jpeg`
+</script>
+
+<div class="index flex flex-col items-center justify-center space-y-4 h-screen">
+    <h1 class="text-5xl font-bold">PixelPalette 0.1</h1>
+
+    <div class="painting-prompt h-1/2">
+        <img src="{paintingPromptPath}" alt="{paintingPromptPath}" class="h-full">
+    </div>
+
+    <div class="answer-picker flex flex-col space-y-4">
+        <button class="border-2 border-black px-4 py-2 hover:bg-teal-200">optie 1</button>
+        <button class="border-2 border-black px-4 py-2 hover:bg-teal-200">optie 2</button>
+        <button class="border-2 border-black px-4 py-2 hover:bg-teal-200">optie 3</button>
+        <button class="border-2 border-black px-4 py-2 hover:bg-teal-200">optie 4</button>
+    </div>
+</div>
+
+<style></style>
